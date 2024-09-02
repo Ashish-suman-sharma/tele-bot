bf36a49abf2c463e072f440318b07bef626a896e
Ashish suman <121541957+Ashish-suman-sharma@users.noreply.github.com>
2024-09-02 16:51:04 +0530

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -1,12 +1,12 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot("7306962151:AAGU0Rm_agYfLCSXScptj_UZaQgBcseiG8Q")
+bot = telebot.TeleBot("7521429771:AAF9RCwrDosdPeBuyeLIsbjdAKbglOZ0dfo")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = types.InlineKeyboardMarkup()
-    web_app = types.WebAppInfo("https://ashishsuman.me/pepe-backup/")
+    web_app = types.WebAppInfo("https://google-clone-rust-nine.vercel.app/")
     button = types.InlineKeyboardButton("Search and Earn", web_app=web_app)
     markup.add(button)
     bot.send_message(message.chat.id, 
